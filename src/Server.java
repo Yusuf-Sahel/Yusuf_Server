7fdbf1da38429b9ac2388ae6a96db7078321c563
yusuf.sahel <yusuf.sahel@elev.ga.ntig.se>
2024-05-28 14:41:22 +0200

    Yusuf_server

--- src/Server.java
+++ src/Server.java
@@ -3,3 +3,14 @@ import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 
+public class Server {
+
+    public static void main(String[] args) throws IOException{
+        int port = 1234;
+        if (args.length > 0){
+            port = Integer.parseInt(args[0]);
+        }
+
+    MulticastSocket socket = new MulticastSocket(port);
+    }
+}
